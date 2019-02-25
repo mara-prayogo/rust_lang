a019e73d6c3a6c4598ad00b361227e4e16260b87
Sohil Ladhani <supertakumi86@gmail.com>
2019-02-25 20:31:51 +0530

    Use of 'const' keyword
    Learned how to use the 'const' keyword and how it is different from
    default immutable variables.

--- chapter_03/variables/src/main.rs
+++ chapter_03/variables/src/main.rs
@@ -1,7 +1,27 @@
 fn main() {
+    
+    /* Variables */
+
     /* let x = 5; */
+
+    /* 'mut' makes a variable mutable. All the variables are immutable
+     * by default */
     let mut x = 5;
     println!("Value of x is {}", x);
     x = 6;
     println!("Value of x is {}", x);
+
+    /* Constants */
+
+    /* Constants are a bit similar to the immutable variables with some major 
+     * differences. 
+     * 1. Constants must be annotated with a type. Using 'let' would not work. 
+     * 2. Constants are always immutable, not just immutable by default. 
+     * 3. Constants can be set only to a constant expression, not to the result
+     * of a function call or any other value that could be computed at runtime.
+     * */
+
+    const SPEED_OF_LIGHT: u32 = 300_000; // Unit in km/s
+    println!("Speed of light is {} km/s.", SPEED_OF_LIGHT);
+
 }
