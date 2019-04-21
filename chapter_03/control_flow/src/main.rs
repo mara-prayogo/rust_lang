d9a851ffc80365973406120835ffaf76ff145198
Sohil Ladhani <supertakumi86@gmail.com>
2019-04-21 21:53:25 +0530

    Till how to use 'if' in a let statment

--- chapter_03/control_flow/src/main.rs
+++ chapter_03/control_flow/src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+   let num = 3;
+   
+   /* good ol' if-else */
+   if num < 5 {
+       println!("num is less than 5");
+   } else {
+       println!("num is not less than 5");
+   }
+
+   /* The condition must always be 'bool' otherwise rust will throw error */
+   /*if num {
+       println!("num is 3");
+   }*/
+   /* shadowing of 'num' variable */
+   let num = 7;
+
+   if num % 4 == 0 {
+       println!("num is divisible by 4");
+   } else if num % 3 == 0 {
+       println!("num is divisible by 3");
+   } else if num % 2 == 0 {
+       println!("num is divisible by 2");
+   } else {
+       println!("num is not divisible by 4,3,2");
+   }
+
+   /* We can use if on the right side of the if statement
+    * Meaning a variable is assigned a value if the condition holds*/
+
+   let condition = true;
+   let number = if condition {
+       5
+   } else {
+       6
+   };
+
+   println!("The value of number is: {}", number);
 }
