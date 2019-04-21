a6b05727e9ffd45a33daafaa0db4555eaf807cd9
Sohil Ladhani <supertakumi86@gmail.com>
2019-04-21 23:11:32 +0530

    Added 'loops' section of Chapter 3

---
+++ chapter_03/loops/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    /* Using 'loop' construct */
+    /* Runs forever until explicitly told to be stopped */
+
+    let mut loop_var = 0;
+    loop {
+        if loop_var == 5 {
+            break;
+        }
+        println!("again!");
+        loop_var += 1;
+    }
+
+    /* Conditional loops using 'while' */
+    /* Countdown code */
+    loop_var = 3;
+    
+    while loop_var != 0 {
+        println!("{}!", loop_var);
+
+        loop_var -= 1;
+    }
+
+    /* Looping through a collection using 'for' */
+    let array = [10, 20, 30, 40, 50];
+
+    let mut index = 0;
+
+    /* How it is done using while loop */
+    /* But this approach is error prone; we could cause the program to panic
+     * if the index length is incorrect. It's also slow, because the compiler
+     * adds runtime code to perform the conditional check on every element on
+     * every iteration through the loop.*/
+    while index < 5 {
+        println!("value at index: {} is: {}", index, array[index]);
+
+        index += 1;
+    }
+
+    /* More concise alternative is to use 'for' loop and execute code for
+     * each item in the collection */
+    for element in array.iter(){
+        println!("The value is: {}", element);
+    }
+
+    /* Countdown code using 'for' loop */
+    /* Note that (1..4). It is a Range type provided by standard library
+     * (a..b) -> [a,b). i.e. a is inclusive and b is exclusive 
+     * 'rev' is used to reversed the Range */
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+}
